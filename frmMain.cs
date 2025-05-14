6735a38f4bc264a23c3682d5168b0b5c6de7c56f
Alumno <Alumno@PC54>
2025-05-14 12:40:03 -0300

    Agregar archivos de proyecto.

---
+++ frmMain.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryControlAcceso
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AbrirVentanaArchivos();
+        }
+
+        public void AbrirVentanaArchivos()
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                lblArchivoSeleccionado.Text = openFileDialog1.FileName;
+            }
+           
+        }
+
+        private void abrirToolStripButton_Click(object sender, EventArgs e)
+        {
+            AbrirVentanaArchivos();
+        }
+    }
+}
